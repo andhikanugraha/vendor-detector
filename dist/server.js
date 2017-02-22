37801e2af163466ede4b7fcfc4c87ead6450905a
Andhika Nugraha <andhika.nugraha@outlook.com>
2017-02-22 16:25:53 +0700

    Added /api endpoint

--- dist/server.js
+++ dist/server.js
@@ -5,25 +5,18 @@ const Search_1 = require("./Search");
 const VendorManager_1 = require("./VendorManager");
 const template_1 = require("./template");
 const app = express();
-app.use('/css', express.static(__dirname + '/../node_modules/bootstrap/dist/css'));
-app.use('/js', express.static(__dirname + '/../node_modules/bootstrap/dist/js'));
-app.use('/', express.static(__dirname + '/../public'));
-app.get('/', (req, res, next) => tslib_1.__awaiter(this, void 0, void 0, function* () {
-    let q = req.query.q;
-    try {
-        let data;
-        if (q) {
-            if (!q.match(/^http/)) {
-                q = 'http://' + q;
+function outJson(req, res, next) {
+    return tslib_1.__awaiter(this, void 0, void 0, function* () {
+        let q = req.query.q;
+        try {
+            if (q) {
+                if (!q.match(/^http/)) {
+                    q = 'http://' + q;
+                }
+                res.locals.data = yield Search_1.detectVendors(q);
             }
-            data = yield Search_1.detectVendors(q);
-        }
-        if (req.accepts('html')) {
-            res.send(template_1.template({ q, data }));
-        }
-        else {
             res.set('Content-type', 'application/json');
-            res.send(JSON.stringify(data, (k, v) => {
+            res.send(JSON.stringify(res.locals.data, (k, v) => {
                 if (v instanceof RegExp) {
                     return {
                         regex: v.toString()
@@ -32,11 +25,33 @@ app.get('/', (req, res, next) => tslib_1.__awaiter(this, void 0, void 0, functio
                 return v;
             }, 2));
         }
+        catch (e) {
+            res.status(500).json({ error: e.getMessage() });
+        }
+    });
+}
+app.use('/css', express.static(__dirname + '/../node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/../node_modules/bootstrap/dist/js'));
+app.use('/', express.static(__dirname + '/../public'));
+app.get('/', (req, res, next) => tslib_1.__awaiter(this, void 0, void 0, function* () {
+    if (!req.accepts('html')) {
+        return outJson(req, res, next);
+    }
+    let q = req.query.q;
+    try {
+        if (q) {
+            if (!q.match(/^http/)) {
+                q = 'http://' + q;
+            }
+            res.locals.data = yield Search_1.detectVendors(q);
+        }
+        res.send(template_1.template({ q, data: res.locals.data }));
     }
     catch (e) {
         res.send(template_1.template({ q, e }));
     }
 }));
+app.get('/api', outJson);
 VendorManager_1.VendorManager.getInstance().init().then(() => {
     Search_1.Search.inited = true;
     app.listen(process.env.PORT || 3000, () => console.log('Express now listening'));
--- src/server.ts
+++ src/server.ts
@@ -6,42 +6,57 @@ import { template } from './template';
 
 const app = express();
 
+async function outJson(req, res, next): Promise<void> {
+  let q = req.query.q;
+  try {
+    if (q) {
+      if (!q.match(/^http/)) {
+        q = 'http://' + q;
+      }
+      res.locals.data = await detectVendors(q);
+    }
+
+    res.set('Content-type', 'application/json');
+    res.send(JSON.stringify(res.locals.data, (k, v) => {
+      if (v instanceof RegExp) {
+        return {
+          regex: v.toString()
+        };
+      }
+
+      return v;
+    }, 2));
+  }
+  catch (e) {
+    res.status(500).json({ error: e.getMessage() });
+  }
+}
+
 app.use('/css', express.static(__dirname + '/../node_modules/bootstrap/dist/css'));
 app.use('/js', express.static(__dirname + '/../node_modules/bootstrap/dist/js'));
 app.use('/', express.static(__dirname + '/../public'));
 
 app.get('/', async(req, res, next) => {
+  if (!req.accepts('html')) {
+    return outJson(req, res, next);
+  }
   let q = req.query.q;
   try {
-    let data;
     if (q) {
       if (!q.match(/^http/)) {
         q = 'http://' + q;
       }
-      data = await detectVendors(q);
+      res.locals.data = await detectVendors(q);
     }
 
-    if (req.accepts('html')) {
-      res.send(template({q, data}));
-    }
-    else {
-      res.set('Content-type', 'application/json');
-      res.send(JSON.stringify(data, (k, v) => {
-        if (v instanceof RegExp) {
-          return {
-            regex: v.toString()
-          };
-        }
-
-        return v;
-      }, 2));
-    }
+    res.send(template({q, data: res.locals.data}));
   }
   catch (e) {
     res.send(template({ q, e }));
-    // next(e);
   }
 });
+
+app.get('/api', outJson);
 
 VendorManager.getInstance().init().then(() => {
   Search.inited = true;
